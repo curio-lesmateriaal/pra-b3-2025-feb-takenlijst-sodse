f3f03f1d7e4bd4fb74ae0f1be930a40d62af6016
SemTheDutchMan <semgoossens09@gmail.com>
2025-03-13 09:53:31 +0100

    Header, logo en stylesheet gemaakt (nog niet af)

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container-header">
+        <nav>
+            <img src="img/logo.png"  alt="logo" class="logo">
+            <a href="index.php">Home</a> |
+            <a href="tasks/index.php">Meldingen</a>
+        </nav>
+        <div>
+            <a href="#" style="color: lightgrey;">Inloggen</a>
+        </div>
+    </div>
+</header>
