9050a9be8e01a8d252b555c5a77ad18795c421f7
Meand05 <djay.verlaar@icloud.com>
2025-03-25 10:55:04 +0100

    sprint 1 (COMPLETE)

--- tasks/index.php
+++ tasks/index.php
@@ -5,9 +5,8 @@
 <head>
     <title>TakenLijst / Overzicht</title>
     <?php require_once "../head.php" ?>
+    <link rel="stylesheet" href="style.css">
 </head>
-
-
 
 <body>
     <?php require_once "../header.php" ?>
@@ -20,128 +19,88 @@
 
     <div class="msg">
         <?php
-        //DIT IS VOOR MELDING TE TONEN!!!!
+        // Dit is voor het tonen van meldingen
         if (isset($_GET['msg'])) {
-
             $message = $_GET['msg'];
-
-
             echo "<p>$message</p>";
         }
         ?>
     </div>
 
     <div class="container">
+        <!-- To-Do -->
         <div class="to-do">
             <h1>To do</h1>
             <?php
-            //1. Verbinding
             require_once '../backend/conn.php';
 
-            //2. Query
-            $query = "SELECT * FROM  taken WHERE status = 'todo'";
+            // Query om taken op te halen met status 'todo'
+            $query = "SELECT * FROM taken WHERE status = 'todo'";
 
-            //3. Prepare
             $statement = $conn->prepare($query);
-            //4. Execute
             $statement->execute();
-
-            //5. Fetch
             $takenToDo = $statement->fetchAll(PDO::FETCH_ASSOC);
             ?>
 
-
-
-
-
-            <?php foreach ($takenToDo as $taak): ?>
-                <ul>
-
-                    <li><a href="edit.php?id=<?php echo $taak['id']; ?>" class="tasks-ind"><span
-                                class="afdeling-done"><?php echo $taak['titel']; ?> Afdeling:
-                                <?php echo $taak['afdeling']; ?></a></li>
-
-
-
-                </ul>
-
-
-            <?php endforeach; ?>
+            <ul>
+                <?php foreach ($takenToDo as $taak): ?>
+                    <li>
+                        <a href="edit.php?id=<?php echo $taak['id']; ?>" class="tasks-ind">
+                            <span class="afdeling-done">
+                                <?php echo $taak['titel']; ?> - Afdeling: <?php echo $taak['afdeling']; ?>
+                            </span>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
 
-
+        <!-- In-Progress -->
         <div class="in-progress">
             <h1>In progress</h1>
             <?php
-            //1. Verbinding
-            require_once '../backend/conn.php';
+            $query = "SELECT * FROM taken WHERE status = 'in-progress'";
 
-            //2. Query
-            $query = "SELECT * FROM  taken WHERE status = 'in-progress'";
-
-            //3. Prepare
             $statement = $conn->prepare($query);
-            //4. Execute
             $statement->execute();
-
-            //5. Fetch
             $takenInProgress = $statement->fetchAll(PDO::FETCH_ASSOC);
             ?>
 
-
-
-
-
-            <?php foreach ($takenInProgress as $taak): ?>
-                <ul>
-                    <li><a href="edit.php?id=<?php echo $taak['id']; ?>" class="tasks-ind2"><span
-                                class="afdeling-done"><?php echo $taak['titel']; ?> Afdeling:
-                                <?php echo $taak['afdeling']; ?></a></li>
-                </ul>
-
-
-            <?php endforeach; ?>
+            <ul>
+                <?php foreach ($takenInProgress as $taak): ?>
+                    <li>
+                        <a href="edit.php?id=<?php echo $taak['id']; ?>" class="tasks-ind2">
+                            <span class="afdeling-done">
+                                <?php echo $taak['titel']; ?> - Afdeling: <?php echo $taak['afdeling']; ?>
+                            </span>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
 
+        <!-- Done -->
         <div class="done">
             <h1>Done</h1>
             <?php
-            //1. Verbinding
-            require_once '../backend/conn.php';
+            $query = "SELECT * FROM taken WHERE status = 'done'";
 
-            //2. Query
-            $query = "SELECT * FROM  taken WHERE status = 'done'";
-
-            //3. Prepare
             $statement = $conn->prepare($query);
-            //4. Execute
             $statement->execute();
-
-            //5. Fetch
             $takenDone = $statement->fetchAll(PDO::FETCH_ASSOC);
             ?>
 
-
-
-
-
-            <?php foreach ($takenDone as $taak): ?>
-                <ul>
-                    <li><a href="edit.php?id=<?php echo $taak['id']; ?>" class="tasks-ind3"><span
-                                class="afdeling-done"><?php echo $taak['titel']; ?> Afdeling:
-                                <?php echo $taak['afdeling']; ?></a></li>
-
-                </ul>
-
-
-            <?php endforeach; ?>
-
-
-
-
-
-
-
+            <ul>
+                <?php foreach ($takenDone as $taak): ?>
+                    <li>
+                        <a href="edit.php?id=<?php echo $taak['id']; ?>" class="tasks-ind3">
+                            <span class="afdeling-done">
+                                <?php echo $taak['titel']; ?> - Afdeling: <?php echo $taak['afdeling']; ?>
+                            </span>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     </div>
 </body>
